aae98fad5283bad63e00ca2f2f8ccaf7a3341cc3
DuchkPy <36851093+DuchkPy@users.noreply.github.com>
2020-06-08 15:00:40 +0200

    Affichage du nombre de station sélectionnée

--- core/class/prixcarburants.class.php
+++ core/class/prixcarburants.class.php
@@ -217,143 +217,55 @@ class prixcarburants extends eqLogic {
 	public function postInsert() {
 		$mynewcolis = new prixcarburants();
 		
-		$top1adr = null;
-		$top1adr = new prixcarburantsCmd();
-		$top1adr->setName('Top 1 Adresse');
-		$top1adr->setEqLogic_id($this->getId());
-		$top1adr->setSubType('string');
-		$top1adr->setType('info');
-		$top1adr->setIsHistorized(0);
-		$top1adr->setIsVisible(1);
-		$top1adr->setDisplay('showNameOndashboard',0);
-		$top1adr->save();
+		$nbstation = $unvehicule->getConfiguration('nbstation','3');
 		
-		$top1prix = null;
-		$top1prix = new prixcarburantsCmd();
-		$top1prix->setName('Top 1 Prix');
-		$top1prix->setEqLogic_id($this->getId());
-		$top1prix->setSubType('numeric');
-		$top1prix->setType('info');
-		$top1prix->setIsHistorized(0);
-		$top1prix->setIsVisible(1);
-		$top1prix->setUnite('€/L');
-		$top1prix->setDisplay('showNameOndashboard',0);
-		$top1prix->setTemplate('dashboard','badge');
-		$top1prix->setTemplate('mobile','badge');
-		$top1prix->save();
-		
-		$top1maj = null;
-		$top1maj = new prixcarburantsCmd();
-		$top1maj->setName('Top 1 MAJ');
-		$top1maj->setEqLogic_id($this->getId());
-		$top1maj->setSubType('string');
-		$top1maj->setType('info');
-		$top1maj->setIsHistorized(0);
-		$top1maj->setDisplay('showNameOndashboard',0);
-		$top1maj->setIsVisible(1);
-		$top1maj->save();
-		
-		$top1id = null;
-		$top1id = new prixcarburantsCmd();
-		$top1id->setName('Top 1 ID');
-		$top1id->setEqLogic_id($this->getId());
-		$top1id->setSubType('string');
-		$top1id->setType('info');
-		$top1id->setIsHistorized(0);
-		$top1id->setIsVisible(0);
-		$top1id->save();
-		
-		$top2adr = null;
-		$top2adr = new prixcarburantsCmd();
-		$top2adr->setName('Top 2 Adresse');
-		$top2adr->setEqLogic_id($this->getId());
-		$top2adr->setSubType('string');
-		$top2adr->setType('info');
-		$top2adr->setIsHistorized(0);
-		$top2adr->setDisplay('showNameOndashboard',0);
-		$top2adr->setIsVisible(1);
-		$top2adr->save();
-		
-		$top2prix = null;
-		$top2prix = new prixcarburantsCmd();
-		$top2prix->setName('Top 2 Prix');
-		$top2prix->setEqLogic_id($this->getId());
-		$top2prix->setSubType('numeric');
-		$top2prix->setType('info');
-		$top2prix->setIsHistorized(0);
-		$top2prix->setIsVisible(1);
-		$top2prix->setDisplay('showNameOndashboard',0);
-		$top2prix->setUnite('€/L');
-		$top2prix->setTemplate('dashboard','badge');
-		$top2prix->setTemplate('mobile','badge');
-		$top2prix->save();
-		
-		$top2maj = null;
-		$top2maj = new prixcarburantsCmd();
-		$top2maj->setName('Top 2 MAJ');
-		$top2maj->setEqLogic_id($this->getId());
-		$top2maj->setSubType('string');
-		$top2maj->setType('info');
-		$top2maj->setIsHistorized(0);
-		$top2maj->setIsVisible(1);
-		$top2maj->setDisplay('showNameOndashboard',0);
-		$top2maj->save();
-		
-		$top2id = null;
-		$top2id = new prixcarburantsCmd();
-		$top2id->setName('Top 2 ID');
-		$top2id->setEqLogic_id($this->getId());
-		$top2id->setSubType('string');
-		$top2id->setType('info');
-		$top2id->setIsHistorized(0);
-		$top2id->setIsVisible(0);
-		$top2id->save();
-		
-		$top3adr = null;
-		$top3adr = new prixcarburantsCmd();
-		$top3adr->setName('Top 3 Adresse');
-		$top3adr->setEqLogic_id($this->getId());
-		$top3adr->setSubType('string');
-		$top3adr->setType('info');
-		$top3adr->setIsHistorized(0);
-		$top3adr->setIsVisible(1);
-		$top3adr->setDisplay('showNameOndashboard',0);
-		$top3adr->save();
-		
-		$top3prix = null;
-		$top3prix = new prixcarburantsCmd();
-		$top3prix->setName('Top 3 Prix');
-		$top3prix->setEqLogic_id($this->getId());
-		$top3prix->setSubType('numeric');
-		$top3prix->setType('info');
-		$top3prix->setIsHistorized(0);
-		$top3prix->setIsVisible(1);
-		$top3prix->setDisplay('showNameOndashboard',0);
-		$top3prix->setTemplate('dashboard','badge');
-		$top3prix->setTemplate('mobile','badge');
-		$top3prix->setUnite('€/L');
-		$top3prix->save();
-		
-		$top3maj = null;
-		$top3maj = new prixcarburantsCmd();
-		$top3maj->setName('Top 3 MAJ');
-		$top3maj->setEqLogic_id($this->getId());
-		$top3maj->setSubType('string');
-		$top3maj->setType('info');
-		$top3maj->setIsHistorized(0);
-		$top3maj->setIsVisible(1);
-		$top3maj->setDisplay('showNameOndashboard',0);
-		$top3maj->save();
-		
-		$top3id = null;
-		$top3id = new prixcarburantsCmd();
-		$top3id->setName('Top 3 ID');
-		$top3id->setEqLogic_id($this->getId());
-		$top3id->setSubType('string');
-		$top3id->setType('info');
-		$top3id->setIsHistorized(0);
-		$top3id->setIsVisible(0);
-		$top3id->save();
+		For($i = 0; $i < $nbstation; $i++) {
+		    $topadr[$i] = null;
+		    $topadr[$i] = new prixcarburantsCmd();
+		    $topadr[$i]->setName('Top 1 Adresse');
+		    $topadr[$i]->setEqLogic_id($this->getId());
+		    $topadr[$i]->setSubType('string');
+		    $topadr[$i]->setType('info');
+		    $topadr[$i]->setIsHistorized(0);
+		    $topadr[$i]->setIsVisible(1);
+		    $topadr[$i]->setDisplay('showNameOndashboard',0);
+		    $topadr[$i]->save();
+		    
+		    $topprix[$i] = null;
+		    $topprix[$i] = new prixcarburantsCmd();
+		    $topprix[$i]->setName('Top 1 Prix');
+		    $topprix[$i]->setEqLogic_id($this->getId());
+		    $topprix[$i]->setSubType('numeric');
+		    $topprix[$i]->setType('info');
+		    $topprix[$i]->setIsHistorized(0);
+		    $topprix[$i]->setIsVisible(1);
+		    $topprix[$i]->setUnite('€/L');
+		    $topprix[$i]->setDisplay('showNameOndashboard',0);
+		    $topprix[$i]->setTemplate('dashboard','badge');
+		    $topprix[$i]->setTemplate('mobile','badge');
+		    $topprix[$i]->save();
+		    
+		    $topmaj[$i] = null;
+		    $topmaj[$i] = new prixcarburantsCmd();
+		    $topmaj[$i]->setName('Top 1 MAJ');
+		    $topmaj[$i]->setEqLogic_id($this->getId());
+		    $topmaj[$i]->setSubType('string');
+		    $topmaj[$i]->setType('info');
+		    $topmaj[$i]->setIsHistorized(0);
+		    $topmaj[$i]->setDisplay('showNameOndashboard',0);
+		    $topmaj[$i]->setIsVisible(1);
+		    $topmaj[$i]->save();
+		    
+		    $topid[$i] = null;
+		    $topid[$i] = new prixcarburantsCmd();
+		    $topid[$i]->setName('Top 1 ID');
+		    $topid[$i]->setEqLogic_id($this->getId());
+		    $topid[$i]->setSubType('string');
+		    $topid[$i]->setType('info');
+		    $topid[$i]->setIsHistorized(0);
+		    $topid[$i]->setIsVisible(0);
+		    $topid[$i]->save();
+		}
 	}
 
 	public function preSave() {}
