94304a2f8bff7b641c810afa2c78778c4493f3bd
DuchkPy <36851093+DuchkPy@users.noreply.github.com>
2022-03-12 23:12:25 +0100

    Remove old unused files and folders
    Suppression des anciens fichiers présent dans core/class, à la fin de la fonction _update()

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -19,66 +19,67 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 require_once dirname(__FILE__) . '/../core/class/prixcarburants.class.php';
 
-function prixcarburants_checkCron(){
+function prixcarburants_checkCron() {
   // update du cron ocazou
-  $freq = config::byKey('freq','prixcarburants');
-  $perso= config::byKey('autorefresh','prixcarburants');
-  log::add('prixcarburants','debug', 'la valeur de config est : '.config::byKey('freq','prixcarburants'));
-  if(($freq==null || $freq== '') || ($freq=='prog' && ($perso==null || $perso == ''))){
-    config::save('freq',prixcarburants::DEFAULT_CRON,'prixcarburants');//dayly
+  $freq = config::byKey('freq', 'prixcarburants');
+  $perso = config::byKey('autorefresh', 'prixcarburants');
+  log::add('prixcarburants', 'debug', 'la valeur de config est : ' . config::byKey('freq', 'prixcarburants'));
+  if (($freq == null || $freq == '') || ($freq == 'prog' && ($perso == null || $perso == ''))) {
+    config::save('freq', prixcarburants::DEFAULT_CRON, 'prixcarburants'); //dayly
   }
-  if(config::byKey('freq_geo','prixcarburants')==null){
-    config::save('freq_geo','event','prixcarburants');
+  if (config::byKey('freq_geo', 'prixcarburants') == null) {
+    config::save('freq_geo', 'event', 'prixcarburants');
   }
-  prixcarburants::setUpdateCron();// mise en place du cron selon la config ou par défaut
+  prixcarburants::setUpdateCron(); // mise en place du cron selon la config ou par défaut
 }
 
 function prixcarburants_install() {
-  log::add('prixcarburants','debug', '======= installation');
+  log::add('prixcarburants', 'debug', '======= installation');
   prixcarburants::updatePrixCarburant();
   prixcarburants_checkCron();
-
 }
 
 function prixcarburants_update() {
   $plugin = plugin::byId('prixcarburants');
   foreach (eqLogic::byType($plugin->getId()) as $eqLogic) {
-      log::add('prixcarburants','debug', '!!======= mise à jour plugin ');
-      
-      
-      // geoloc parameter to fit previous config
-      $geoloc = $eqLogic->getConfiguration('geoloc', 'none');
-      log::add('prixcarburants','debug', 'current geoloc :'.$geoloc);
+    log::add('prixcarburants', 'debug', '!!======= mise à jour plugin ');
 
-  		if($geoloc == 'jeedom'){// si jeedom 
-        	$eqLogic->setConfiguration('jeedom_loc', true);
-          $eqLogic->setConfiguration('geoloc', null);
-          $eqLogic->setConfiguration('auto_update', false);
-          log::add('prixcarburants','debug', 'ajout de la localisation jeedom ');
-          
-        }elseif(is_numeric($geoloc) && !is_null($geoloc)){// si cmd id
-          $eqLogic->setConfiguration('jeedom_loc', false);
-          $locationcmd = cmd::byEqLogicIdAndLogicalId($geoloc, 'location:coordinate');
-          if(!is_object($locationcmd)){
-            log::add('prixcarburants','debug', 'essai commande localisation par ID');
-            $locationcmd = cmd::byId($geoloc);
-          }
-          if(is_object($locationcmd)){
-           $eqLogic->setConfiguration('geoloc', "#". $locationcmd->getId()."#");
-           $eqLogic->setConfiguration('auto_update', false);
-            log::add('prixcarburants','debug', 'transformation de la commande localisation : '.$locationcmd->getHumanName());
-          }else{
-            log::add('prixcarburants','debug', 'commande localisation non trouvée');
-          }
-          
-        }
-		$eqLogic->save();
- }
- // check cron 
- prixcarburants::updatePrixCarburant();
- prixcarburants_checkCron();
- log::add('prixcarburants','debug','=============  fin de mise à jour');
 
+    // geoloc parameter to fit previous config
+    $geoloc = $eqLogic->getConfiguration('geoloc', 'none');
+    log::add('prixcarburants', 'debug', 'current geoloc :' . $geoloc);
+
+    if ($geoloc == 'jeedom') { // si jeedom 
+      $eqLogic->setConfiguration('jeedom_loc', true);
+      $eqLogic->setConfiguration('geoloc', null);
+      $eqLogic->setConfiguration('auto_update', false);
+      log::add('prixcarburants', 'debug', 'ajout de la localisation jeedom ');
+    } elseif (is_numeric($geoloc) && !is_null($geoloc)) { // si cmd id
+      $eqLogic->setConfiguration('jeedom_loc', false);
+      $locationcmd = cmd::byEqLogicIdAndLogicalId($geoloc, 'location:coordinate');
+      if (!is_object($locationcmd)) {
+        log::add('prixcarburants', 'debug', 'essai commande localisation par ID');
+        $locationcmd = cmd::byId($geoloc);
+      }
+      if (is_object($locationcmd)) {
+        $eqLogic->setConfiguration('geoloc', "#" . $locationcmd->getId() . "#");
+        $eqLogic->setConfiguration('auto_update', false);
+        log::add('prixcarburants', 'debug', 'transformation de la commande localisation : ' . $locationcmd->getHumanName());
+      } else {
+        log::add('prixcarburants', 'debug', 'commande localisation non trouvée');
+      }
+    }
+    $eqLogic->save();
+  }
+  // check cron 
+  prixcarburants::updatePrixCarburant();
+  prixcarburants_checkCron();
+  log::add('prixcarburants', 'debug', '=============  fin de mise à jour');
+
+  //Remove unused files
+  if (file_exists("/../core/class/stations.json")) unlink("/../core/class/stations.json");
+  if (file_exists("/../core/class/PrixCarburants_instantane.xml")) unlink("/../core/class/PrixCarburants_instantane.xml");
+  if (file_exists("/../core/class/listestations")) deleteTree("/../core/class/listestations");
 }
 
 
@@ -86,12 +87,22 @@ function prixcarburants_remove() {
   // remove of cron job
   $cron = cron::byClassAndFunction('prixcarburants', 'udpateAllData');
   if (is_object($cron)) {
-      $cron->remove();
+    $cron->remove();
   }
   $cron = cron::byClassAndFunction(__CLASS__, 'pullGeoCmd');
   if (is_object($cron)) {
     $cron->remove();
-}
+  }
 }
 
-?>
+/** Function to delete files and folders */
+function deleteTree($dir) {
+  foreach (glob($dir . "/*") as $element) {
+    if (is_dir($element)) {
+      deleteTree($element);
+      rmdir($element);
+    } else {
+      unlink($element);
+    }
+  }
+}
